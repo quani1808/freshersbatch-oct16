344ea561c856eabb6f1274b9b6ba0e535b927a7a
quani1808 <115769212+quani1808@users.noreply.github.com>
2023-01-18 15:02:39 +0530

    Add files via upload

---
+++ Diseases-Services-Application/Diseases-Services-Application/src/main/java/com/gl/diseasesservice/DiseasesServicesApplication/DiseasesServicesApplication.java
@@ -0,0 +1,13 @@
+package com.gl.diseasesservice.DiseasesServicesApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiseasesServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiseasesServicesApplication.class, args);
+	}
+
+}
---
+++ Diseases-Services-Application/Diseases-Services-Application/src/test/java/com/gl/diseasesservice/DiseasesServicesApplication/DiseasesServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.diseasesservice.DiseasesServicesApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiseasesServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
