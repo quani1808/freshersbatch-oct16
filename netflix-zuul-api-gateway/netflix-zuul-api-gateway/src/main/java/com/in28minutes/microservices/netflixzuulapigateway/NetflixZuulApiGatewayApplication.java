d382c768d4f7196f670d66b850e0e4d1c64ab245
quani1808 <115769212+quani1808@users.noreply.github.com>
2023-01-18 14:51:42 +0530

    Add files via upload

---
+++ netflix-zuul-api-gateway/netflix-zuul-api-gateway/src/main/java/com/in28minutes/microservices/netflixzuulapigateway/NetflixZuulApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.netflixzuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayApplication.class, args);
+	}
+
+}
