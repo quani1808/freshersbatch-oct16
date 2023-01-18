cd1c6b480e161d7f3c33efeeed2ace4eec2f1141
quani1808 <115769212+quani1808@users.noreply.github.com>
2023-01-18 14:57:09 +0530

    Add files via upload

---
+++ movie-info-service/movie-info-service/src/main/java/io/javabrains/movieinfoservice/model/Movie.java
@@ -0,0 +1,27 @@
+package io.javabrains.movieinfoservice.model;
+
+public class Movie {
+	
+	public Movie(String movieId, String name) {
+		super();
+		this.movieId = movieId;
+		this.name = name;
+	}
+	private String movieId;
+	private String name;
+	public String getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
